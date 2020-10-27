d1def2aa0b43a2339915ab01cd1d4b2bd207d76b
hansong8675 <hansong8675@gmail.com>
2020-10-27 20:57:12 +0700

    SwiftUI的经典模版项目

--- TemplateProjectSwiftUI/TemplateProjectSwiftUI/TabbarView.swift
+++ TemplateProjectSwiftUI/TemplateProjectSwiftUI/TabbarView.swift
@@ -23,24 +23,44 @@ struct TabbarView: View {
     var body: some View {
         TabView(selection: $selectedTab,
                 content:  {
-                    HomeView().tabItem {
-                        Image(systemName: "list.dash")
-                        Text("Home")
+                    Text("q").tabItem {
+                                                Image(systemName: "list.dash")
+                                                Text("HomeA")
                         
-                    }.tag(1)
-                    CommunityView().tabItem {
-                        Image(systemName: "square.and.pencil")
-                        Text("活动")
-                            .foregroundColor(.orange)
-                            .font(.subheadline)
-                    }.tag(2)
-                    DiscoverView().tabItem {
-                        Image(systemName: "pencil.and.outline")
-                        Text("新人") }.tag(3)
-                    Mine().tabItem {
-                        Image(systemName: "signature")
-                        Text("记录")
-                    }.tag(4)
+                                            }.tag(1)
+                    Text("q").tabItem {
+                                                Image(systemName: "list.dash")
+                                                Text("HomeB")
+                        
+                                            }.tag(2)
+                    Text("qw").tabItem {
+                                                Image(systemName: "list.dash")
+                                                Text("HomeC")
+                        
+                                            }.tag(3)
+                    Text("as").tabItem {
+                                                Image(systemName: "list.dash")
+                                                Text("HomeD")
+                        
+                                            }.tag(4)
+//                    HomeView().tabItem {
+//                        Image(systemName: "list.dash")
+//                        Text("Home")
+//
+//                    }.tag(1)
+//                    CommunityView().tabItem {
+//                        Image(systemName: "square.and.pencil")
+//                        Text("活动")
+//                            .foregroundColor(.orange)
+//                            .font(.subheadline)
+//                    }.tag(2)
+//                    DiscoverView().tabItem {
+//                        Image(systemName: "pencil.and.outline")
+//                        Text("新人") }.tag(3)
+//                    Mine().tabItem {
+//                        Image(systemName: "signature")
+//                        Text("记录")
+//                    }.tag(4)
                 })
     }
 }
