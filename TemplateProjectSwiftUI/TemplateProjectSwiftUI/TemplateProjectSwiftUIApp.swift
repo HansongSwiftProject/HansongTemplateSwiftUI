cd73514586e2c691671ca4a2019b23e7bbd47564
hansong8675 <hansong8675@gmail.com>
2020-10-27 14:35:05 +0700

    项目初始化

---
+++ TemplateProjectSwiftUI/TemplateProjectSwiftUI/TemplateProjectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateProjectSwiftUIApp.swift
+//  TemplateProjectSwiftUI
+//
+//  Created by hansong on 10/27/20.
+//
+
+import SwiftUI
+
+@main
+struct TemplateProjectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
